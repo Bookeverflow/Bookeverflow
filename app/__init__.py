5c592845df16f28c5f43491cdfd9be582bd08815
kit <sa951.@gmail.com>
2017-01-21 15:22:31 +0800

    Try to add login function

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,24 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_openid import OpenID
+from config import basedir
+
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
 
 from app import views, models
